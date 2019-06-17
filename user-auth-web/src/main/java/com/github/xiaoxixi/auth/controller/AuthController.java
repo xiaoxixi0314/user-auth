b35d9a0906d9332b28dabe4592d487883ef82c28
xiaoxixi <xiaoxixi@caibaopay.com>
2019-06-17 22:53:00 +0800

    login logout implements

--- user-auth-web/src/main/java/com/github/xiaoxixi/auth/controller/AuthController.java
+++ user-auth-web/src/main/java/com/github/xiaoxixi/auth/controller/AuthController.java
@@ -5,7 +5,9 @@ import com.github.xiaoxixi.auth.enums.ErrorCodeEnum;
 import com.github.xiaoxixi.auth.exception.BizException;
 import com.github.xiaoxixi.auth.result.Result;
 import com.github.xiaoxixi.auth.service.LoginService;
+import com.github.xiaoxixi.auth.utils.LoginUtils;
 import com.github.xiaoxixi.auth.vo.UserSessionVO;
+import com.xiaoxixi.service.register.redis.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,6 +30,9 @@ public class AuthController{
 
     @Autowired
     private LoginService loginService;
+
+    @Autowired
+    private RedisService redisService;
 
     @PostMapping("/login")
     public Result<UserSessionVO> login(@RequestParam String userCode,
@@ -60,17 +69,22 @@ public class AuthController{
     }
 
     @PostMapping("/logout")
-    public Result<Boolean> logout(Long userId, HttpServletResponse response){
+    public Result<Boolean> logout(HttpServletRequest request, HttpServletResponse response){
         try {
-            // clear cookie
-            Cookie cookie = new Cookie(Constants.TOKEN_COOKIE_NAME, "");
+            // clear token in redis
+            Cookie cookie = WebUtils.getCookie(request, Constants.TOKEN_COOKIE_NAME);
+            if (Objects.isNull(cookie)){
+                return Result.error(ErrorCodeEnum.ILLEGAL_USER);
+            }
+            String accessToken = cookie.getValue();
+            cookie = new Cookie(Constants.TOKEN_COOKIE_NAME, "");
             cookie.setMaxAge(0);
             cookie.setHttpOnly(true);
             cookie.setPath("/");
             response.addCookie(cookie);
-            // clear token in redis
-
-
+            // clear redis token
+            String tokenRedisKey = LoginUtils.buildRedisTokenKey(accessToken);
+            redisService.delete(tokenRedisKey);
             return Result.success(Boolean.TRUE);
         } catch (Exception e) {
             LOGGER.error("logout system error:", e);
--- user-auth-web/src/main/java/com/github/xiaoxixi/auth/utils/LoginUtils.java
+++ user-auth-web/src/main/java/com/github/xiaoxixi/auth/utils/LoginUtils.java
@@ -4,9 +4,9 @@ import com.github.xiaoxixi.auth.contants.Constants;
 import com.github.xiaoxixi.auth.domain.UserAccessToken;
 import com.github.xiaoxixi.auth.enums.ErrorCodeEnum;
 import com.github.xiaoxixi.auth.exception.ParamsException;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.Objects;
 
 public class LoginUtils {
@@ -19,18 +19,16 @@ public class LoginUtils {
         if (Objects.isNull(token)) {
             throw new ParamsException(ErrorCodeEnum.PARAM_CANT_EMPTY);
         }
-        Long userId = token.getUserId();
         String accessToken = token.getAccessToken();
-        return buildRedisTokenKey(userId, accessToken);
+        return buildRedisTokenKey(accessToken);
     }
 
-    public static String buildRedisTokenKey(Long userId, String accessToken) {
-        if (Objects.isNull(userId) || Objects.isNull(accessToken)) {
+    public static String buildRedisTokenKey(String accessToken) {
+        if (StringUtils.isEmpty(accessToken)) {
             throw new ParamsException(ErrorCodeEnum.PARAM_CANT_EMPTY);
         }
         return new StringBuilder().append(Constants.REDIS_TOKEN_PREFIX)
                 .append(Constants.REDIS_KEY_SPLIT)
-                .append(userId)
                 .append(accessToken)
                 .toString();
     }
